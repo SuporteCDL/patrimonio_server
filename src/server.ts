796769e65a1932ef573ab416fb6ffbcf30ba0404
SuporteCDL <suportecdlanapolis@gmail.com>
2025-10-10 15:08:58 -0300

    Updated Cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import Fastify from 'fastify'
 import { registerRoutes } from './app/routes'
+import cors from '@fastify/cors'
 
 const app = Fastify({
   logger: true
 })
 
+app.register(cors, {
+  origin: true,
+})
 app.register(registerRoutes)
 
 app.listen({ host: '0.0.0.0', port: 3333 }).then(() => {
